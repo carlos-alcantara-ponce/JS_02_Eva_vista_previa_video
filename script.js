ab9c1f70a6268b7a268454d32b7ba545b99da6b9
Carlos.Ernesto.A <calcanta_peru@hotmail.com>
2023-06-07 16:44:09 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// console.log("page loaded...");
+
+// alert("enter script...");
+
+function fpreview() {
+
+    // console.log("enter fpreview...");
+    var xvid = document.getElementById("my-video");
+
+
+    xvid.play();
+
+
+
+}
+
+function fstop() {
+
+    // console.log("enter fstop...");
+
+    var xvid = document.getElementById("my-video");
+    xvid.pause();
+
+}
+
+function fplay() {
+
+    // console.log("enter fstop...");
+
+    var xvid = document.getElementById("my-video");
+    xvid.play();
+
+}
